82f9a646956b8c88704f0d3035574dc6f5c16eca
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-05-13 12:14:33 +0200

    Index.html works

--- Hangman2/src/main/java/hu/tetra_meno_septemes/app/MainPage.java
+++ Hangman2/src/main/java/hu/tetra_meno_septemes/app/MainPage.java
@@ -1,5 +1,4 @@
 package hu.tetra_meno_septemes.app;
-import com.sun.net.httpserver.HttpServer;
 import hu.tetra_meno_septemes.app.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -12,13 +11,10 @@ import javax.servlet.http.*;
 @WebServlet(urlPatterns = {"/"})
 public class MainPage extends HttpServlet {
 
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    public void doGet(HttpServletRequest req,
-                      HttpServletResponse resp) throws ServletException, IOException {
-
-       /* TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-        engine.process("templates/index.html", context, resp.getWriter());*/
-       resp.getWriter().println("ldshflasdflsajfjsf");
-    }
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+            WebContext context = new WebContext(req, resp, req.getServletContext());
+            engine.process("/index.html", context, resp.getWriter());
+        }
 }
