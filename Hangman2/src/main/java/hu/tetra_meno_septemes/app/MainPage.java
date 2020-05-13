795d30ec2dae0d814834a701d8d9c363a9fece6d
marthon-szabo <makotoimx@gmail.com>
2020-05-13 14:27:06 +0200

    Add .gitignore

--- Hangman2/src/main/java/hu/tetra_meno_septemes/app/MainPage.java
+++ Hangman2/src/main/java/hu/tetra_meno_septemes/app/MainPage.java
@@ -1,5 +1,8 @@
 package hu.tetra_meno_septemes.app;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import hu.tetra_meno_septemes.app.config.TemplateEngineUtil;
+import hu.tetra_meno_septemes.app.word.Word;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -15,8 +18,30 @@ public class MainPage extends HttpServlet {
 
             TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
             WebContext context = new WebContext(req, resp, req.getServletContext());
-            engine.process("/index.html", context, resp.getWriter());
-        }
+            engine.process("index.html", context, resp.getWriter());
+    }
 
 
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Word word = new Word();
+        Gson stringFromJS = new Gson();
+
+        BufferedReader bufferedReader = req.getReader();
+        Word requestToJson = stringFromJS.fromJson(bufferedReader, Word.class);
+
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        builder.excludeFieldsWithoutExposeAnnotation();
+        Gson gson = builder.create();
+        String productsToMainPage = gson.toJson(requestToJson.startGame());
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().print(productsToMainPage);
+        resp.getWriter().flush();
+
+
+
+    }
 }
